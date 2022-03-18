f18639197285751b6396d77ab4a8ef18e20c4ef2
Ronambulo <101815831+Ronambulo@users.noreply.github.com>
2022-03-18 12:08:36 +0100

    Update and rename 18,03,2022.py to Mark-Calculator.py

---
+++ Class Exercices/Mark-Calculator.py
@@ -0,0 +1,28 @@
+mark = float(input("mark: "))
+
+if (mark<5):
+    print("suspenso") #F in Spain
+
+else:
+    if (mark<6):
+        print("suficiente") #D in Spain
+
+    else:
+        if (mark<7):
+            print("bien") #C in Spain
+        
+        else:
+           
+            if (mark<9):
+                print("notable") #B in Spain
+
+            else:
+
+                if (mark<=10):
+                    print("sobresaliente") #A in Spain
+
+                else:
+                    print("tu nota no es valida, esta por encima de 10")
+
+
+            
